84823a892826b3c74f1fa6f0e9d6d75aca0cbe03
jaemin2 <fkdlem524@naver.com>
2022-04-04 21:49:48 +0900

    dto에 Validation 어노테이션 추가

--- src/main/java/com/sesac/foodtruckitem/ui/dto/request/StoreRequestDto.java
+++ src/main/java/com/sesac/foodtruckitem/ui/dto/request/StoreRequestDto.java
@@ -1,5 +1,6 @@
 package com.sesac.foodtruckitem.ui.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import com.sesac.foodtruckitem.infrastructure.persistence.mysql.entity.*;
@@ -8,6 +9,10 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
 public class StoreRequestDto {
 
     /**
@@ -15,7 +20,7 @@ public class StoreRequestDto {
      * @author jaemin
      * @version 1.0.0
      * 작성일 2022-04-03
-    **/
+     **/
     @JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
     @Data
     @Builder
@@ -24,20 +29,50 @@ public class StoreRequestDto {
     public static class CreateStoreDto {
 
         private Long userId;
+
+        @NotBlank(message = "가게명을 입력해주세요")
         private String storeName;
+
+        @NotBlank(message = "가게 전화번호를 입력해주세요")
         private String phoneNum;
+
+        @NotBlank(message = "공지를 입력해주세요")
         private String notice;
+
+        @NotBlank(message = "카테고리를 입력해주세요")
         private String categoryName;
+
+        // JsonFormat을 이용해 직렬화
+        @NotBlank(message = "영업시간을 입력해주세요")
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:MM:ss", timezone = "Asia/Seoul")
+        private LocalDateTime openTime;//영업 시간
+
         private String imgName;
+
         private String imgUrl;
+
+        @NotBlank(message = "영업장소를 입력해주세요")
         private String city;
+
+        @NotNull(message = "영업장소를 입력해주세요")
         private String street;
+
+        @NotNull(message = "우편번호를 입력해주세요")
         private String zipCode;
+
         private Double latitude;
+
         private Double longitude;
+
+        @NotBlank(message = "사업자 등록번호를 입력해주세요")
         private String bNo; // 사업자 등록 번호
+
+        @NotBlank(message = "개업일을 입력해주세요")
         private String sDt; // 개업일
+
+        @NotBlank(message = "대표자 성명을 입력해주세요")
         private String pName; // 대표자 성명
+
 //        private Category category;
 //        private Images images;
 //        private Address address;
